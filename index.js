e24b396e87f5630f3e8b86f289136682f6f73cbe
picassoeason <easonwu.student@outlook.com>
2024-01-02 16:04:39 +0800

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ admin.initializeApp({
 // 中間件
 app.use(cors());
 app.use(bodyParser.json());
-app.post('/api/hello', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
     const { name } = req.body;
     res.status(200).json({ message: `Hello ${name}!` });
